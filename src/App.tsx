bd449c8c0a17627e8b9d5d3d5742413e09a0f625
CapSap <cshotam@gmail.com>
2022-01-27 16:32:44 +1100

    test tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom";
 
 import Entry from "./components/Entry";
 
 const App = () => {
   return (
-    <div>
+    <div className="p-0 m-0">
       <h1>App</h1>
       <Entry />
     </div>
