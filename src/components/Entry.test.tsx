58bbcb86294931522fc652041c620cd825a039c6
CapSap <cshotam@gmail.com>
2022-01-27 16:10:31 +1100

    jest appears to be working

---
+++ src/components/Entry.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Entry from "./Entry";
+
+describe("entry comp", () => {
+  it("exists", () => {
+    const entry = <Entry />;
+    expect(entry).toBeDefined();
+  });
+
+  it("has a form elelemt that users can input values", () => {
+    const entry = <Entry />;
+  });
+});
